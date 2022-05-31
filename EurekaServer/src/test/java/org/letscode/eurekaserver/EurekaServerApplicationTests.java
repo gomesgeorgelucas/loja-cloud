6375c95b1752d6a3ad43b21c3b63de0f77c5094f
MVMATOS <marcus.viniciusv@sonda.com>
2022-05-30 21:19:59 -0300

    config eureka

---
+++ EurekaServer/src/test/java/org/letscode/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.letscode.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
