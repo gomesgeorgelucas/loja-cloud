b09f9933e4144026ecf1a7ba291877ba0af79b5f
MVMATOS <marcus.viniciusv@sonda.com>
2022-05-30 21:33:27 -0300

    update in eureka server

--- servico-produto/src/main/java/org/letscode/servicoproduto/ServicoProdutoApplication.java
+++ servico-produto/src/main/java/org/letscode/servicoproduto/ServicoProdutoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServicoProdutoApplication {
 
     public static void main(String[] args) {
