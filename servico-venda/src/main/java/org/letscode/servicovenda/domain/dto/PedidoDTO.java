c9cc5fbe4769ce1826cc092789cc012c9505516f
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-06-01 10:19:20 -0400

    ammend-missing pedido dto

--- servico-venda/src/main/java/org/letscode/servicovenda/domain/dto/PedidoDTO.java
+++ servico-venda/src/main/java/org/letscode/servicovenda/domain/dto/PedidoDTO.java
@@ -1,4 +1,15 @@
 package org.letscode.servicovenda.domain.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
 public class PedidoDTO {
+    private Long vendaId;
+    private Long produtoId;
+    private Long produtoQuantidade;
+    private BigDecimal produtoPreco;
 }
